65e9bedfbf2e1981c58013c71aaa8c2f879a46df
stanly <stanlyjavier21@outlook.es>
2023-10-09 23:01:47 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const os = require('node:os')
+
+// const fs = require('node:fs')
+
+// const state = fs.statSync('./archivo.txt')
+// console.log(state)
+
+console.log("Inbformacion del sistema:", os.platform())
+
+// function sum(a, b) {
+//     return a + b
+// }
+
+// module.exports = sum
