b0a8a971aabd4147d76c5ab62cae58e81bcfa07f
stanly <stanlyjavier21@outlook.es>
2023-10-10 13:18:30 -0400

    Sin errores

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const os = require('node:os')
+const fs = require('node:fs')
 
-// const fs = require('node:fs')
+const state = fs.statSync('./archivo.txt')
+console.log(state)
 
-// const state = fs.statSync('./archivo.txt')
-// console.log(state)
+console.log('Information system:', os.platform())
 
-console.log("Inbformacion del sistema:", os.platform())
+function sum (a, b) {
+  return a + b
+}
 
-// function sum(a, b) {
-//     return a + b
-// }
-
-// module.exports = sum
+module.exports = sum
